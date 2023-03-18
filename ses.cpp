5fe5168e9312948c03626b50ab7604a26f87d62d
PHAM DUY TIEN <duytien@MacBook-Pro-cua-PHAM.local>
2023-03-19 00:11:14 +0700

    add vector clock

--- ses.cpp
+++ ses.cpp
@@ -11,13 +11,21 @@ const int N = 3; // Number of processes
 const int M = 2; // Number of messages per process
 const int T = 2; // Number of threads per process
 
+
 mutex mtx;
 condition_variable cv;
 vector<queue<pair<int, int>>> buffers(N);
 vector<int> counters(N);
+vector<vector<int>> vectorClocks(N, vector<int>(N, 0));
 
 void send(int i, int j, int k) {
     unique_lock<mutex> lock(mtx);
+    vectorClocks[i][i]++;
+    cout << "Process " << i+1 << " sends message " << k << " to process " << j+1 << " with vector clock: [ ";
+    for (int l = 0; l < N; l++) {
+        cout << vectorClocks[i][l] << " ";
+    }
+    cout << "]" << endl;
     buffers[j].push(make_pair(i, k));
     cv.notify_all();
 }
@@ -28,16 +36,43 @@ void receive(int i) {
         cv.wait(lock, [i] { return !buffers[i].empty(); });
         pair<int, int> msg = buffers[i].front();
         buffers[i].pop();
+        vectorClocks[i][i] = max(vectorClocks[i][i], vectorClocks[msg.first][i]);
+        for (int j = 0; j < N; j++) {
+            vectorClocks[i][j] = max(vectorClocks[i][j], vectorClocks[msg.first][j]);
+        }
         if (msg.second == counters[msg.first]) {
-            cout << "Process " << i << " received message " << msg.second << " from process " << msg.first << endl;
+            cout << "Process " << i+1 << " received message " << msg.second << " from process " << msg.first+1
+                 << " (VC: [";
+            for (int j = 0; j < N; j++) {
+                cout << vectorClocks[i][j];
+                if (j < N - 1) {
+                    cout << ", ";
+                }
+            }
+            cout << "])" << endl;
             counters[msg.first]++;
         } else {
-            cout << "Process " << i << " received message " << msg.second << " from process " << msg.first << " (buffered)" << endl;
-            cout << "Process " << i << " is waiting for message " << counters[msg.first] << " from process " << msg.first << endl;
+            cout << "Process " << i+1 << " received message " << msg.second << " from process " << msg.first+1
+                 << " (buffered, VC: [";
+            for (int j = 0; j < N; j++) {
+                cout << vectorClocks[i][j];
+                if (j < N - 1) {
+                    cout << ", ";
+                }
+            }
+            cout << "])" << endl;
+            cout << "Process " << i+1 << " is waiting for message " << counters[msg.first] << " from process " << msg.first+1
+                 << " (VC: [";
+            for (int j = 0; j < N; j++) {
+                cout << vectorClocks[i][j];
+                if (j < N - 1) {
+                    cout << ", ";
+                }
+            }
+            cout << "])" << endl;
         }
     }
 }
-
 void process(int i) {
     vector<thread> threads;
     for (int j = 0; j < T; j++) {
